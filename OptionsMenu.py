b0f3e1831b412bff727e65d350bea43b76ab80d0
SomethingRandom <shishkebab259@gmail.com>
2022-01-20 18:27:16 -0600

    Options: Added head-changing slider func

--- OptionsMenu.py
+++ OptionsMenu.py
@@ -1,4 +1,3 @@
-from typing import Text
 from direct.gui import DirectGui
 from direct.gui.DirectGui import *
 from direct.gui.DirectGuiGlobals import FLAT, HORIZONTAL, SUNKEN, VERTICAL
@@ -21,11 +20,13 @@ options_geom = 'phase_3/models/gui/ttr_m_gui_gen_buttons.bam'
 class OptionsMenu:
     '''The main OptionsMenu
        Houses the DirectFrame that is the entire frame.'''
-    def __init__(self):
+    def __init__(self, toon):
         self.main_geom = loader.loadModel('phase_3/models/gui/ttr_m_gui_sbk_settingsPanel.bam')
         self.options_geom = loader.loadModel(options_geom)
         self.icon = loader.loadModel('phase_3/models/gui/toontown-logo.bam')
         self.toontaskicons = loader.loadModel('phase_3.5/models/gui/ttr_m_gui_qst_toontask_icons.bam')
+
+        self.selectedToon = toon
 
         # Removing nodes that aren't needed
 
@@ -118,21 +119,79 @@ class OptionsMenu:
 
         def updateHead():
             '''Updates the Toon's head based on the value'''
-            sliderValue = self.first_slider.slider['value']
-            print(sliderValue)
+            sliderValue = self.head_slider.slider['value']
+            tested_value = int(sliderValue)
+
+            if (self.selectedToon.head.head_type[0] + self.selectedToon.head.head_type[1]) == 'ca': # If the Toon is a cat
+                if tested_value < 20 and tested_value > 0:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('css')
+                elif tested_value < 40 and tested_value > 20:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('csl')
+                elif tested_value < 60 and tested_value > 40:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('cls')
+                elif tested_value > 60:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('cll')
+
+            elif self.selectedToon.head.head_type[0] == 'b': # If the Toon is a bear 
+                if tested_value < 20 and tested_value > 0:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('bss')
+                elif tested_value < 40 and tested_value > 20:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('bsl')
+                elif tested_value < 60 and tested_value > 40:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('bls')
+                elif tested_value > 60:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('bll')
+            
+            elif (self.selectedToon.head.head_type[0] + self.selectedToon.head.head_type[1]) == 'cr': # If the Toon is a crocodile 
+                if tested_value < 20 and tested_value > 0:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('crss')
+                elif tested_value < 40 and tested_value > 20:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('crsl')
+                elif tested_value < 60 and tested_value > 40:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('crls')
+                elif tested_value > 60:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('crll')
+
+            elif (self.selectedToon.head.head_type[0] + self.selectedToon.head.head_type[1]) == 'de': # If the Toon is a crocodile 
+                if tested_value < 20 and tested_value > 0:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('dess')
+                elif tested_value < 40 and tested_value > 20:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('desl')
+                elif tested_value < 60 and tested_value > 40:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('dels')
+                elif tested_value > 60:
+                    self.selectedToon.head.removeHead()
+                    self.selectedToon.head.generateHead('dell')
+
+
+            #print(int(sliderValue))
         
         def changeGender():
             print('test2')
         
-        self.musicLabel = OptionsLabel(self.optionsScroll.getCanvas(),'Music',  0.8)
-        self.first_slider= OptionsSlider(self.optionsScroll.getCanvas(), 'True Friends:', 0.65)
-        self.second_slider= OptionsSlider(self.optionsScroll.getCanvas(), 'Big Brains:', 0.50)
-        self.toonDNALabel = OptionsLabel(self.optionsScroll.getCanvas(),'Toon DNA',  0.35)
+        #self.musicLabel = OptionsLabel(self.optionsScroll.getCanvas(),'Test',  0.8)
+        self.toonDNALabel = OptionsLabel(self.optionsScroll.getCanvas(),'Toon DNA',  0.8)
+        self.head_slider= OptionsSlider(self.optionsScroll.getCanvas(), 'Head:', 0.7, updateHead)
         self.first_toggle= OptionsToggle(self.optionsScroll.getCanvas(), 'Test:', 0.20)
-        self.clothingLabel = OptionsLabel(self.optionsScroll.getCanvas(),'Clothing',  0)
-        self.second_toggle= OptionsToggle(self.optionsScroll.getCanvas(), 'Clothing Tes:', -0.20)
-        self.accessoryLabel = OptionsLabel(self.optionsScroll.getCanvas(),'Accessories',  -0.4)
-
+        self.clothingLabel = OptionsLabel(self.optionsScroll.getCanvas(),'Test:',  0)
+        self.second_toggle= OptionsToggle(self.optionsScroll.getCanvas(), 'Test:', -0.20)
+        self.accessoryLabel = OptionsLabel(self.optionsScroll.getCanvas(),'Test',  -0.4)
+        #self.test_menu = OptionsChoosingMenu(self.optionsScroll.getCanvas(), 'Test:', -0.7) 
 
 class OptionsLabel:
     '''Used as labels for the bigger letters
@@ -177,6 +236,7 @@ class OptionsModal(DirectGui.DirectFrame):
         self.modalTextNode.reparentTo(self.containerFrame)
 
 class OptionsSlider(OptionsModal):
+    '''Creates a Slider which is useful for functions with arguments that include a range'''
     def __init__(self, modalParent, modalText, z, slider_command=None):
         super().__init__(modalParent, modalText, z) # Creates the text on the left
         self.options_geom = loader.loadModel('phase_3/models/gui/ttr_m_gui_gen_buttons.bam')
@@ -198,6 +258,7 @@ class OptionsSlider(OptionsModal):
         self.slider.setPos(1.3,0,0)
 
 class OptionsToggle(OptionsModal):
+    '''Creates a toggle that creates an off/on switch'''
     def __init__(self, modalParent, modalText, z, toggle_command=None, fake_command=None):
         super().__init__(modalParent, modalText, z) # Creates the text on the left
         self.options_geom = loader.loadModel('phase_3/models/gui/ttr_m_gui_gen_buttons.bam')
@@ -238,6 +299,69 @@ class OptionsToggle(OptionsModal):
             else:
                 toggle_back_interval = LerpPosInterval(self.toggle_thumb_geom, 0.15, (0.35,0,-0.25), (0.65,0,-0.25) )
                 toggle_back_interval.start()
+
+class OptionsChoosingMenu(OptionsModal):
+    '''Creates a menu with a bunch of options'''
+    def __init__(self, modalParent, modalText, z, width=None):
+        super().__init__(modalParent, modalText, z)
+        dynamicFrameFile = loader.loadModel('phase_3/models/gui/ttr_m_gui_gen_dynamicFrame.bam')
+
+        self.dynamic_frame = NodePath('test')
+
+        # The Top Left piece
+        self.top_leftdf = dynamicFrameFile.find('**/*topLeft')
+        self.top_leftdf.setScale(0.05)
+        self.top_leftdf.reparentTo(self.dynamic_frame)
+        self.top_leftdf.setPos(0.5,0,0.5)
+
+        # The top middle piece
+        top_middle_model = dynamicFrameFile.find('**/*topMiddle')
+        top_middle_model.reparentTo(self.top_leftdf)
+        top_middle_model.setPos(1,0,0)
+
+        # The Top Middle repetitions
+        for i in range(1, 10):
+            self.copyModel = top_middle_model.copyTo(self.top_leftdf)
+            self.copyModel.setPos(top_middle_model.getX()+i,0,0)
+
+        # The Top Right piece
+        top_right_model = dynamicFrameFile.find('**/topRight')
+        top_right_model.reparentTo(self.copyModel)
+        top_right_model.setPos(1,0,0)
+
+        # The entire middle portion
+        #for i in range(1, 10):
+        #    self.center_leftdf = dynamicFrameFile.find('**/*centerLeft')
+        #    self.center_leftdf.setScale(0.05)
+        #    self.center_leftdf.reparentTo(self.dynamic_frame)
+        #    self.center_leftdf.setPos(0.5,0,-1)
+
+        #self.center_rightdf = dynamicFrameFile.find('**/*centerRight')
+        #self.center_rightdf.setScale(1)
+        #self.center_rightdf.reparentTo(self.center_middledf)
+        #self.center_rightdf.setPos(1,0,0)
+
+        #self.bottom_leftdf = dynamicFrameFile.find('**/*bottomLeft')
+        #self.bottom_leftdf.setScale(1)
+        #self.bottom_leftdf.reparentTo(self.center_middledf)
+        #self.bottom_leftdf.setPos(-1,0,-1)
+
+        #self.bottom_middledf = dynamicFrameFile.find('**/*bottomMiddle')
+        #self.bottom_middledf.setScale(1)
+        #self.bottom_middledf.reparentTo(self.center_middledf)
+        #self.bottom_middledf.setPos(0,0,-1)
+
+        #self.bottom_rightdf = dynamicFrameFile.find('**/*bottomRight')
+        #self.bottom_rightdf.setScale(1)
+        #self.bottom_rightdf.reparentTo(self.center_middledf)
+        #self.bottom_rightdf.setPos(1,0,-1)
+
+        #self.dynamic_frame.setScale(1.5,1.5,1.5)
+        #self.dynamic_frame.setPos(1.5,0,0)
+        self.dynamic_frame.reparentTo(self.containerFrame)
+    
+
+
 
 
 

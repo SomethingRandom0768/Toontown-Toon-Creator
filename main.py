fff9a5f4aaa5fcd985ccc64f5723110446f54a52
SomethingRandom <shishkebab259@gmail.com>
2022-01-26 08:40:53 -0600

    Main: more arguments to Toon, and some changes using config file

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+
+from panda3d.core import loadPrcFileData
+
+loadPrcFileData('', 'interpolate-frames true')
+loadPrcFileData('', 'fullscreen false')
+loadPrcFileData('', 'window-title Toon Creator')
+
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties
 from OptionsMenu import *
@@ -9,12 +16,12 @@ class MyApp(ShowBase):
 
         # Window settings
         windowSettings = WindowProperties()
-        windowSettings.setFullscreen(False)
         windowSettings.setSize(1280, 720)
+        windowSettings.setIconFilename('phase_3/etc/icon.ico')
         self.win.requestProperties(windowSettings)
 
         # Music
-        self.music = loader.loadSfx('phase_3/audio/bgm/ttr_d_theme_phase1.ogg')
+        self.music = loader.loadSfx('phase_5/audio/bgm/ttr_s_ara_cbe_cogdoStreet.ogg')
         self.music.setLoop(True)
         #self.music.play()
 
@@ -24,11 +31,11 @@ class MyApp(ShowBase):
         self.environment.setHpr(-90,0,0)
 
         # Moving camera
-        base.cam.setPos(5,20,3)
+        base.cam.setPos(5,17.5,3)
         base.cam.setHpr(0,0,0)
         base.disableMouse()
 
-        self.toon = Toon('de', 'ls', None, 's')
+        self.toon = Toon('mi', 'ls', False, 'ss', 's', 'f')
         self.options = OptionsMenu(self.toon)
     
 app = MyApp()

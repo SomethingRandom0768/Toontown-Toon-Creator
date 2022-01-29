f40b7642bf9fabadd6cba2c2d96e61a8af9c160c
SomethingRandom <shishkebab259@gmail.com>
2022-01-28 23:26:37 -0600

    ToonDNA: Adding specific dict and changing this file to be only used for data

--- ToonDNA.py
+++ ToonDNA.py
@@ -1,3 +1,6 @@
+# THIS FILE CONTAINS ALL THE DICTIONARIES USED FOR THE CHOOSING MENUS IN OPTIONSMENU.PY
+from panda3d.core import *
+
 colorsList = {
     'White'        : VBase4(1.0, 1.0, 1.0, 1.0),                 
     'Peach'        : VBase4(0.96875, 0.691406, 0.699219, 1.0),   
@@ -28,31 +31,17 @@ colorsList = {
     'Black'         :VBase4(0.3, 0.3, 0.35, 1.0), 
 }
 
-class ToonDNA():
-    '''This entire class acts as data for the Toon. Toon.py will read from this and create a Toon'''
-    def __init__(self, name='', gender='m', headType=0, torsoType=0, legsType=0, headColor=0, torsoColor=0, legsColor=0, glovesColor=0):
-        self.name = name
-        self.gender = gender 
-        self.headType = headType
-        self.torsoType =  torsoType
-        self.legsType = legsType
-        self.headColor = headColor
-        self.torsoColor = torsoColor
-        self.legsColor = legsColor
-        self.glovesColor = glovesColor
-
-    def __str__(self):
-        dna_string  = f"""
-        Name        : {self.name}
-        Gender      : {self.gender}
-        Head Type   : {self.headType}
-        Torso Type  : {self.torsoType} 
-        Legs Type   : {self.legsType}
-        Head Color  : {self.headColor}
-        Torso Color : {self.torsoColor}
-        Legs Color  : {self.legsColor}
-        Gloves Color: {self.glovesColor}
-        """
-        return dna_string
-        
-object = ToonDNA('Flippy', 'm', 1, 1, 1, 1, 1, 1, 1)
+species_dict = {
+             'Bear'         :'b',
+             'Cat'          :'ca',
+             'Crocodile'    :'cr',
+             'Deer'         :'de',
+             'Dog'          :'d',
+             'Duck'         :'du',
+             'Horse'        :'h',
+             'Monkey'       :'mo',
+             'Mouse'        :'mi',
+             'Pig'          :'p',
+             'Rabbit'       :'r',
+             'Riggy'        :'ri'
+             }

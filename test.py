a6dcb0d5385aa167e5aca6840ebb6c2ae8d37848
SomethingRandom0768 <shishkebab259@gmail.com>
2022-03-25 21:30:00 -0600

    Test: improved green screen mechanic (thanks to Loonatic for this one!)

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-
+from datetime import datetime
 from panda3d.core import loadPrcFileData
-loadPrcFileData('', 'fullscreen false')
+loadPrcFileData('', 'fullscreen true')
 loadPrcFileData('', 'window-title Test File')
 
 from direct.showbase.ShowBase import ShowBase
@@ -13,7 +13,7 @@ class MyApp(ShowBase):
 
         # Window settings
         windowSettings = WindowProperties()
-        windowSettings.setSize(1280, 720)
+        windowSettings.setSize(1920, 1080)
         windowSettings.setIconFilename('phase_3/etc/icon.ico')
         self.win.requestProperties(windowSettings)
 
@@ -23,19 +23,34 @@ class MyApp(ShowBase):
         #self.music.play()
 
         # Moving camera
-        base.cam.setPos(10,-20,3)
+        base.cam.setPos(10,-10,3)
         base.cam.setHpr(0,0,0)
 
 
         # The following Toon's concept was created by Lovealot/Ghosheart, go check her work out at https://toyhou.se/Ghosheart
-        self.toon = Toon('ca', 'ss', True, 'ld', 'l', 'f', 'Yellow', 'Bright Red', 'White', 'Bright Red', 'Scientist A', 'Scientist D Shorts', 'Trainee Skirt', 'White', 'White', None, None, 1, None, None, None, 'Neutral', True, False)
+        self.toon = Toon('du', 'ss', False, 'ls', 's', 'f', 'Yellow', 'Yellow', 'White', 'Yellow', 'Oiled Sellbot Cog-Crusher Shirt', 'Oiled Sellbot Cog-Crusher Shorts', 'Oiled Sellbot Cog-Crusher Skirt', 'White', 'White', None, 'Four Eyes', 1, None, None, 'Sleuthing Sneakers', 'fewaf', True, True)
         self.toon.toonActor.setPos(10,0,0)
-        self.toon.toonActor.pose('Victory Dance', 10)
-        base.setBackgroundColor((57/255),255,(20/255))
+        self.toon.toonActor.pose('Call Pet', 50)
+        base.setBackgroundColor(1,1,1)
 
         # Use the print-screen function on your keyboard to take a screenshot, and then post it in 
         # https://www.aatbio.com/tools/online-automatic-green-screen-remover, a tool you can use to get rid of green screen!
 
+        self.isShadowHidden = False
+
+        def showOrHideShadow():
+            if self.isShadowHidden:
+                self.isShadowHidden = False
+                self.toon.shadow.reparentTo(self.toon.toonActor.find('**/joint_shadow'))
+            else:
+                self.isShadowHidden = True
+                self.toon.shadow.reparentTo(hidden)
+
+        def takeScreenshot():
+            base.screenshot(f'Toon{datetime.now()}.png', False)
+
+        self.accept('space', takeScreenshot)
+        self.accept('s', showOrHideShadow)
 
 app = MyApp()
 app.run()

cea0a1926cc95e43bcfc94941da286aba27451cd
SomethingRandom0768 <shishkebab259@gmail.com>
2022-06-25 02:53:36 -0500

    Accessory placement GUI, removed useless function

--- src/gui/ToonEditorMenu.py
+++ src/gui/ToonEditorMenu.py
@@ -27,14 +27,6 @@ class ToonEditorMenu(EditorMenu):
 
         self.selectedToon = toon
 
-        self.hidingButton = DirectGui.DirectButton(self.menuFrame,
-        frameSize=(-0.15, 1.85, -0.75, 0.75),
-        command=self.hideSelectables,
-        pos=(0,2.5,0),
-        frameColor=(0,0,0,0.1)
-        )
-
-
 # Contains all the modals
 
         self.rotation_slider = OptionsSlider(
@@ -98,24 +90,267 @@ class ToonEditorMenu(EditorMenu):
 
         self.accessoryLabel = OptionsLabel(
             self.thirdPageOptionsScroll.getCanvas(), 'Accessories',  0.8)
-
         self.shoes_switching_slider = OptionsSlider(
             self.thirdPageOptionsScroll.getCanvas(), 'Shoe Type:', -0.4, self.updateShoes)
-
         self.shoes_texture_menu = OptionsChoosingMenu(self.thirdPageOptionsScroll.getCanvas(
         ), 'Shoes:', 0, -0.2, 22, shoe_texture_dict, self.updateShoeTexture, 0)
-
         self.boot_short_texture_menu = OptionsChoosingMenu(self.thirdPageOptionsScroll.getCanvas(
         ), 'Short Boot:', 0, 0, 22, shoe_texture_dict, self.updateShortBootTexture, 0)
-
         self.boot_long_texture_menu = OptionsChoosingMenu(self.thirdPageOptionsScroll.getCanvas(
         ), 'Long Boot:', 0, 0.2, 15, boot_long_texture_dict, self.updateLongBootTexture, 0)
-
         self.glasses_menu = OptionsChoosingMenu(self.thirdPageOptionsScroll.getCanvas(
         ), 'Glasses:', -0.1, 0.4, 22, glasses_dict, self.updateGlasses, 0)
-
         self.backpack_menu = OptionsChoosingMenu(self.thirdPageOptionsScroll.getCanvas(
-        ), 'Backpack:', -0.1, 0.6, 23, backpack_dict, self.updateBackpack, 0)
+        ), 'Backpack:', -0.1, 0.6, 24, backpack_dict, self.updateBackpack, 0)
+
+        textEntryGui = self.optionsGeom.find('**/*ttr_t_gui_gen_buttons_box')
+        hatPositioningLabel = OptionsLabel(self.thirdPageOptionsScroll.getCanvas(), "Hat Placement", -0.75)
+        backpackPositioningLabel = OptionsLabel(self.thirdPageOptionsScroll.getCanvas(), "Backpack Placement", -1.20)
+        glassesPositioningLabel = OptionsLabel(self.thirdPageOptionsScroll.getCanvas(), "Glasses Placement", -1.65)
+# - 3.75
+# Hat Frames
+        self.hatXFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='x position',
+            text_font = self.modalFont,#self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.775, 0, -0.95)
+        )
+        self.hatXEntry = DirectEntry(
+            parent=self.hatXFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.5),
+            relief=None,
+            pos=(-0.5,0,-0.08)
+        )
+        self.hatYFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='y position',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.45, 0, -0.95)
+        )
+        self.hatYEntry = DirectEntry(
+            parent=self.hatYFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.5),
+            relief=None,
+            pos=(-0.5,0,-0.08)
+        )
+        self.hatZFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='z position',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.125, 0, -0.95)
+        )
+        self.hatZEntry = DirectEntry(
+            parent=self.hatZFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.5),
+            relief=None,
+            pos=(-0.5,0,-0.08)
+        )
+        self.hatScaleFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='scale',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (0.150, 0, -0.95)
+        )
+        self.hatScaleEntry = DirectEntry(
+            parent=self.hatScaleFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.5),
+            relief=None,
+            pos=(-0.5,0,-0.08)
+        )
+
+# Backpack frames
+        self.backpackXFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='x position',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.775, 0, -1.40)
+        )
+        self.backpackXEntry = DirectEntry(
+            parent=self.backpackXFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.2),
+            relief=None,
+            command=self.updateBackpackXPos,
+            pos=(-0.5,0,-0.08)
+        )
+        self.backpackYFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='y position',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.45, 0, -1.40)
+        )
+        self.backpackYEntry = DirectEntry(
+            parent=self.backpackYFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.2),
+            relief=None,
+            command=self.updateBackpackYPos,
+            pos=(-0.5,0,-0.08)
+        )
+        self.backpackZFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='z position',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.125, 0, -1.40)
+        )
+        self.backpackZEntry = DirectEntry(
+            parent=self.backpackZFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.2),
+            relief=None,
+            command=self.updateBackpackZPos,
+            pos=(-0.5,0,-0.08)
+        )
+        self.backpackScaleFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='scale',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (0.150, 0, -1.40)
+        )
+        self.backpackScaleEntry = DirectEntry(
+            parent=self.backpackScaleFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.2),
+            relief=None,
+            command=self.updateBackpackScale,
+            pos=(-0.5,0,-0.08)
+        )
+
+# Glasses frames
+
+        self.glassesXFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='x position',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.775, 0, -1.85)
+        )
+        self.glassesXEntry = DirectEntry(
+            parent=self.glassesXFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.2),
+            relief=None,
+            command=self.updateGlassesXPos,
+            pos=(-0.5,0,-0.08)
+        )
+        self.glassesYFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='y position',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.45, 0, -1.85)
+        )
+        self.glassesYEntry = DirectEntry(
+            parent=self.glassesYFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.2),
+            relief=None,
+            command=self.updateGlassesYPos,
+            pos=(-0.5,0,-0.08)
+        )
+        self.glassesZFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='z position',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (-0.125, 0, -1.85)
+        )
+        self.glassesZEntry = DirectEntry(
+            parent=self.glassesZFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.2),
+            relief=None,
+            command=self.updateGlassesZPos,
+            pos=(-0.5,0,-0.08)
+        )
+        self.glassesScaleFrame = DirectFrame(
+            parent = self.thirdPageOptionsScroll.getCanvas(),
+            geom = textEntryGui,
+            relief=None,
+            scale=(0.15,0.15,0.15),
+            text='scale',
+            text_font = self.modalFont,
+            text_scale=0.5,
+            text_pos=(0,0.5,0),
+            pos = (0.150, 0, -1.85)
+        )
+        self.glassesScaleEntry = DirectEntry(
+            parent=self.glassesScaleFrame,
+            text_scale=0.35,
+            text_font = self.modalFont,
+            frameSize=(-0.2,1.25,-0.55,0.2),
+            relief=None,
+            command=self.updateGlassesScale,
+            pos=(-0.5,0,-0.08)
+        )
+
 
 # FOURTH PAGE
 
@@ -318,9 +553,9 @@ class ToonEditorMenu(EditorMenu):
             self.rotation_slider.slider['value'])
 
     def smoothanimationToggle(self):
-        if self.selectedToon.smooth_enabled:
+        if self.selectedToon.smoothEnabled:
             self.selectedToon.toonActor.setBlend(frameBlend=False)
-            self.selectedToon.smooth_enabled = False
+            self.selectedToon.smoothEnabled = False
         else:
             self.selectedToon.toonActor.setBlend(frameBlend=True)
             self.selectedToon.smooth_enabled = True
@@ -333,7 +568,7 @@ class ToonEditorMenu(EditorMenu):
             self.selectedToon.toonActor.find('**/*feet').show()
             self.selectedToon.wearsShoes = False
         else:
-            self.selectedToon.attachShoes(self.selectedToon.shoe_type)
+            self.selectedToon.attachShoes(self.selectedToon.shoeType)
             self.selectedToon.wearsShoes = True
 
     def updateSpecies(self, species):
@@ -356,13 +591,13 @@ class ToonEditorMenu(EditorMenu):
 # Accessory related functions
 
     def updateBackpack(self, backpack_type):
-        self.selectedToon.backpack_type = backpack_type
+        self.selectedToon.backpackType = backpack_type
         self.selectedToon.attachBackpack(backpack_type)
 
-        self.backpackXEntry.set( str( round( self.selectedToon.returnBackpackPosition().getX(), 2 ) ) )
-        self.backpackYEntry.set( str( round( self.selectedToon.returnBackpackPosition().getY(), 2 ) ) )
-        self.backpackZEntry.set( str( round( self.selectedToon.returnBackpackPosition().getZ(), 2 ) ) )
-        self.backpackScaleEntry.set(f"{ round(self.selectedToon.backpack_model.getScale().getX(), 2) } ")
+        # self.backpackXEntry.set( str( round( self.selectedToon.returnBackpackPosition().getX(), 2 ) ) )
+        # self.backpackYEntry.set( str( round( self.selectedToon.returnBackpackPosition().getY(), 2 ) ) )
+        # self.backpackZEntry.set( str( round( self.selectedToon.returnBackpackPosition().getZ(), 2 ) ) )
+        # self.backpackScaleEntry.set(f"{ round(self.selectedToon.backpack_model.getScale().getX(), 2) } ")
 
         self.notify.debug(f"Backpack has been changed to {backpack_type}")
 
@@ -379,13 +614,13 @@ class ToonEditorMenu(EditorMenu):
         self.selectedToon.backpack_model.setScale(float(newScale))
 
     def updateGlasses(self, glasses_type):
-        self.selectedToon.glasses_type = glasses_type
+        self.selectedToon.glassesType = glasses_type
         self.selectedToon.attachGlasses(glasses_type)
         
-        self.glassesXEntry.set( str( round( self.selectedToon.returnGlassesPosition().getX(), 2 ) ) )
-        self.glassesYEntry.set( str( round( self.selectedToon.returnGlassesPosition().getY(), 2 ) ) )
-        self.glassesZEntry.set( str( round( self.selectedToon.returnGlassesPosition().getZ(), 2 ) ) )
-        self.glassesScaleEntry.set(f"{ round(self.selectedToon.glasses_model.getScale().getX(), 2) } ")
+        # self.glassesXEntry.set( str( round( self.selectedToon.returnGlassesPosition().getX(), 2 ) ) )
+        # self.glassesYEntry.set( str( round( self.selectedToon.returnGlassesPosition().getY(), 2 ) ) )
+        # self.glassesZEntry.set( str( round( self.selectedToon.returnGlassesPosition().getZ(), 2 ) ) )
+        # self.glassesScaleEntry.set(f"{ round(self.selectedToon.glasses_model.getScale().getX(), 2) } ")
         self.notify.debug(f"Glasses has been changed to {glasses_type}")
 
     def updateGlassesXPos(self, xPosition):
@@ -403,33 +638,33 @@ class ToonEditorMenu(EditorMenu):
 # Clothing related functions
 
     def updateShirtTexture(self, shirt_texture):
-        self.selectedToon.shirt_texture = shirt_texture
+        self.selectedToon.shirtTexture = shirt_texture
         self.selectedToon.setShirtTexture(shirt_texture)
         self.notify.debug(f"Shirt Texture has been changed to {shirt_texture}")
 
     def updateShortTexture(self, short_texture):
-        self.selectedToon.short_texture = short_texture
+        self.selectedToon.shortTexture = short_texture
         self.selectedToon.setShortTexture(short_texture)
         self.notify.debug(f"Short Texture has been changed to {short_texture}")
 
     def updateSkirtTexture(self, skirt_texture):
-        self.selectedToon.skirt_texture = skirt_texture
+        self.selectedToon.skirtTexture = skirt_texture
         self.selectedToon.setSkirtTexture(skirt_texture)
         self.notify.debug(f"Skirt Texture has been changed to {skirt_texture}")
 
     def updateShirtColor(self, shirt_color):
-        self.selectedToon.shirt_color = shirt_color
+        self.selectedToon.shirtColor = shirt_color
         self.selectedToon.setShirtColor(shirt_color)
         self.notify.debug(f"Shirt color has been changed to {shirt_color}")
 
     def updateBottomColor(self, bottom_color):
-        self.selectedToon.bottom_color = bottom_color
+        self.selectedToon.bottomColor = bottom_color
         self.selectedToon.setBottomColor(bottom_color)
         self.notify.debug(f"Bottom color has been changed to {bottom_color}")
 
     def updateShoeTexture(self, shoe_texture):
         try:
-            self.selectedToon.shoe_texture = shoe_texture
+            self.selectedToon.shoeTexture = shoe_texture
             self.selectedToon.applyShoeTexture(shoe_texture)
             self.notify.debug(f"Shoe has been changed to {shoe_texture}")
         except:
@@ -437,7 +672,7 @@ class ToonEditorMenu(EditorMenu):
 
     def updateShortBootTexture(self, shoe_texture):
         try:
-            self.selectedToon.short_boot_texture = shoe_texture
+            self.selectedToon.shortBootTexture = shoe_texture
             self.selectedToon.applyShortBootTexture(shoe_texture)
             self.notify.debug(f"Short Boots has been changed to {shoe_texture}")
         except:
@@ -445,7 +680,7 @@ class ToonEditorMenu(EditorMenu):
 
     def updateLongBootTexture(self, shoe_texture):
         try:
-            self.selectedToon.long_boot_texture = shoe_texture
+            self.selectedToon.longBootTexture = shoe_texture
             self.selectedToon.applyLongBootTexture(shoe_texture)
             self.notify.debug(f"Long Boots has been changed to {shoe_texture}")
         except:
@@ -457,68 +692,20 @@ class ToonEditorMenu(EditorMenu):
         tested_value = int(sliderValue)
 
         if tested_value >= 0 and tested_value <= 25:
-            self.selectedToon.shoe_type = 4
+            self.selectedToon.shoeType = 4
             self.selectedToon.hideShoePieces()
         elif tested_value > 25 and tested_value <= 50:
-            self.selectedToon.shoe_type = 1
-            self.selectedToon.attachShoes(self.selectedToon.shoe_type)
+            self.selectedToon.shoeType = 1
+            self.selectedToon.attachShoes(self.selectedToon.shoeType)
         elif tested_value > 50 and tested_value <= 75:
-            self.selectedToon.shoe_type = 2
-            self.selectedToon.attachShoes(self.selectedToon.shoe_type)
+            self.selectedToon.shoeType = 2
+            self.selectedToon.attachShoes(self.selectedToon.shoeType)
         else:
-            self.selectedToon.shoe_type = 3
-            self.selectedToon.attachShoes(self.selectedToon.shoe_type)
+            self.selectedToon.shoeType = 3
+            self.selectedToon.attachShoes(self.selectedToon.shoeType)
 
     def generateToon(self):
         '''Just prints out the Toon's toString'''
         self.notify.debug(self.selectedToon)
 
-    def hideSelectables(self):
-        self.shoes_texture_menu.selectablesFrame.hide()
-        self.shoes_texture_menu.clickable.show()
-
-        self.boot_short_texture_menu.selectablesFrame.hide()
-        self.boot_short_texture_menu.clickable.show()
-
-        self.boot_long_texture_menu.selectablesFrame.hide()
-        self.boot_long_texture_menu.clickable.show()
-
-        self.glasses_menu.selectablesFrame.hide()
-        self.glasses_menu.clickable.show()
-
-        self.backpack_menu.selectablesFrame.hide()
-        self.backpack_menu.clickable.show()
-
-        self.glove_color_menu.selectablesFrame.hide()
-        self.glove_color_menu.clickable.show()
-
-        self.leg_color_menu.selectablesFrame.hide()
-        self.leg_color_menu.clickable.show()
-
-        self.arm_color_menu.selectablesFrame.hide()
-        self.arm_color_menu.clickable.show()
-
-        self.head_color_menu.selectablesFrame.hide()
-        self.head_color_menu.clickable.show()
-
-        self.anim_menu.selectablesFrame.hide()
-        self.anim_menu.clickable.show()
-
-        self.species_menu.selectablesFrame.hide()
-        self.species_menu.clickable.show()
-        
-        self.bottom_coloring_menu.selectablesFrame.hide()
-        self.bottom_coloring_menu.clickable.show()
-
-        self.skirts_menu.selectablesFrame.hide()
-        self.skirts_menu.clickable.show()
-
-        self.shorts_menu.selectablesFrame.hide()
-        self.shorts_menu.clickable.show()
-
-        self.shirt_color_menu.selectablesFrame.hide()
-        self.shirt_color_menu.clickable.show()
-
-        self.shirt_menu.selectablesFrame.hide()
-        self.shirt_menu.clickable.show()
 
